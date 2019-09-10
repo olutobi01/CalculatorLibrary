884ab4f2da0ee569630a3a964c2f73713e521759
olutobi adeyemi <olutobi.adeyemi@halliburton.com>
2019-09-10 10:20:30 +0100

    Add unit tests for calculator

--- calculator.py
+++ calculator.py
@@ -1,13 +1,10 @@
-# -*- coding: utf-8 -*-
 """
-Created on Tue Sep 10 09:52:44 2019
-
-@author: H218676
+Calculator library containing basic math operations.
 """
 
 def add(first_term, second_term):
     return first_term + second_term
 
-def subtract(first_term,second_term):
-    return first_term - second_term
 
+def subtract(first_term, second_term):
+    return first_term - second_term
