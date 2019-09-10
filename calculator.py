94712000bfc1a8c02d5ff67ba255db13cbbb458a
olutobi adeyemi <olutobi.adeyemi@halliburton.com>
2019-09-10 09:56:47 +0100

    Add functions for addition and subtraction

---
+++ calculator.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 10 09:52:44 2019
+
+@author: H218676
+"""
+
+def add(first_term, second_term):
+    return first_term + second_term
+
+def subtract(first_term,second_term):
+    return first_term - second_term
+
